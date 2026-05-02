c345a8f10da8fc5b429d7c46141886b8358f4d4f
vineet-proptiger <vineet.mishra@proptiger.com>
2026-05-02 09:31:49 +0530

    feat: add sitemap.js and robots.js configuration files for SEO optimization

--- app/robots.js
+++ app/robots.js
@@ -1,12 +1,12 @@
-const BASE_URL = 'https://www.asbllegacyrtcxroad.in'
+// const BASE_URL = 'https://www.asbllegacyrtcxroad.in'
 
-export default function robots() {
-  return {
-    rules: {
-      userAgent: '*',
-      allow: '/',
-      disallow: ['/api/', '/admin/', '/auth/', '/config/', '/server/', '/.env'],
-    },
-    sitemap: `${BASE_URL}/sitemap.xml`,
-  }
-}
+// export default function robots() {
+//   return {
+//     rules: {
+//       userAgent: '*',
+//       allow: '/',
+//       disallow: ['/api/', '/admin/', '/auth/', '/config/', '/server/', '/.env'],
+//     },
+//     sitemap: `${BASE_URL}/sitemap.xml`,
+//   }
+// }
