87368211638794a052ee968cd1a7e1f75b0db0bf
vineet-proptiger <vineet.mishra@proptiger.com>
2026-05-07 12:02:13 +0530

    feat: add Location component with interactive map and proximity highlights

--- components/Location.jsx
+++ components/Location.jsx
@@ -82,7 +82,7 @@ const Location = () => (
             position: 'relative',
           }}>
             <iframe
-              src="https://www.google.com/maps/embed?pb=!1m14!1m12!1m3!1d1963546.9773004348!2d78.35763222880459!3d27.985812094564423!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!5e0!3m2!1sen!2sin!4v1777715011137!5m2!1sen!2sin"
+              src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2890.758528891412!2d77.53095467450333!3d28.471383891382175!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x390ceb6973560ebd%3A0x4c079cd6b0fc8edb!2sGodrej%20Golf%20Links%2C%20Greater%20Noida!5e1!3m2!1sen!2sin!4v1778135446334!5m2!1sen!2sin"
               width="100%" height="100%"
               style={{ border: 0, minHeight: '420px', display: 'block', filter: 'grayscale(20%)' }}
               allowFullScreen="" loading="lazy" referrerPolicy="no-referrer-when-downgrade"
