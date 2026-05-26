3f1a71530d1d56c593d7b7b682ab63c1c90db45a
vineet-proptiger <vineet.mishra@proptiger.com>
2026-05-26 16:51:41 +0530

    some fixes

--- app/page.js
+++ app/page.js
@@ -58,6 +58,7 @@ export default function Home() {
       {/* Mobile sticky bottom bar */}
       <div className="sticky-bottom-bar">
         <a
+          id="mobile-call"
           href="tel:+919718344024"
           className="flex-1 flex items-center justify-center py-1"
           style={{ background: '#1a1a1a', borderRight: '1px solid #333' }}
